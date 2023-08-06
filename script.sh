2779aa1c345058a746c325702ed03bbd2610bd0c
Cloud <862863826@qq.com>
2023-08-06 17:36:02 +0800

    修改配置

--- script.sh
+++ script.sh
@@ -7,18 +7,18 @@ mv -f package-temp/luci-app-adguardhome package/lean/
 rm -rf package-temp
 
 # Add luci-app-openclash
-git clone https://github.com/vernesong/OpenClash.git package-temp
-mv -f package-temp/luci-app-openclash package/lean/
-rm -rf package-temp
+# git clone https://github.com/vernesong/OpenClash.git package-temp
+# mv -f package-temp/luci-app-openclash package/lean/
+# rm -rf package-temp
 
 # Add luci-theme-opentomcat
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git theme-temp/luci-theme-opentomcat
-rm -rf theme-temp/luci-theme-opentomcat/LICENSE
-rm -rf theme-temp/luci-theme-opentomcat/README.md
-mv -f theme-temp/luci-theme-opentomcat package/lean/
-rm -rf theme-temp
-default_theme='opentomcat'
-sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+# git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git theme-temp/luci-theme-opentomcat
+# rm -rf theme-temp/luci-theme-opentomcat/LICENSE
+# rm -rf theme-temp/luci-theme-opentomcat/README.md
+# mv -f theme-temp/luci-theme-opentomcat package/lean/
+# rm -rf theme-temp
+# default_theme='opentomcat'
+# sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
 
 # Add luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package-temp/lua-maxminddb
@@ -28,6 +28,11 @@ cp -r package-temp/small/* package/lean/
 mv -f package-temp/lua-maxminddb package/lean/
 mv -f package-temp/luci-app-vssr package/lean/
 rm -rf package-temp
+
+# Add luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 # Add luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
